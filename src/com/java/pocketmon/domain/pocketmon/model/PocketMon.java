f8a67688bbf835f420cf4dc273599eda64b520a7
limzzum <chfhddl1234@naver.com>
2024-05-21 15:50:18 +0900

    :sparkles: feat : 포켓몬 클래스 추가

---
+++ src/com/java/pocketmon/domain/pocketmon/model/PocketMon.java
@@ -0,0 +1,75 @@
+package com.java.pocketmon.domain.pocketmon.model;
+
+import com.java.pocketmon.domain.pocketmon.constant.Detail;
+import com.java.pocketmon.domain.pocketmon.constant.ExceptionMessage;
+import com.java.pocketmon.domain.pocketmon.constant.PocketMonInfo;
+import com.java.pocketmon.domain.pocketmon.dto.ResponseInfoDto;
+
+public class PocketMon {
+    private String name;
+    private int exp;
+    private int power;
+    private int performanceCnt;
+    private Detail detail;
+
+    public PocketMon(String name){
+        this.name = name;
+        this.power = 100;
+    }
+
+    public String performance() {
+        if(performanceCnt == 0){
+            return ExceptionMessage.NOT_ENOUGH_PERFORMANCE;
+        }
+        performanceCnt -=1;
+        return PocketMonInfo.getByName(detail.getName()).getPerformanceMessage();
+    }
+
+    public void eatSnack(){
+        performanceCnt += 1;
+        power += 10;
+    }
+
+    public String cry(){
+        power -= 1;
+        return PocketMonInfo.getByName(detail.getName()).getCryMessage();
+    }
+
+    public void getDamage(int attack){
+        power = Math.max(power - attack, 0);
+    }
+
+    public boolean isAdventageous(PocketMon opponent){
+        int myPriority = detail.getType().getPriority();
+        int oppPriority = opponent.getDetail().getType().getPriority();
+        return myPriority < oppPriority && oppPriority - myPriority == 1;
+    }
+
+    public ResponseInfoDto getInfo(){
+        return new ResponseInfoDto(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getPerformanceCnt() {
+        return performanceCnt;
+    }
+
+    public Detail getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Detail detail) {
+        this.detail = detail;
+    }
+}
