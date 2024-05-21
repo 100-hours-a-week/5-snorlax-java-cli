00ee45d11fe0ac3dc51c16d2b9f00d4c1f55299b
limzzum <chfhddl1234@naver.com>
2024-05-21 15:56:36 +0900

    :sparkles: feat : 포켓몬 정보 응답 dto 추가

---
+++ src/com/java/pocketmon/domain/pocketmon/dto/ResponseInfoDto.java
@@ -0,0 +1,47 @@
+package com.java.pocketmon.domain.pocketmon.dto;
+
+import com.java.pocketmon.domain.pocketmon.constant.Detail;
+import com.java.pocketmon.domain.pocketmon.constant.PocketMonInfo;
+import com.java.pocketmon.domain.pocketmon.model.PocketMon;
+
+public class ResponseInfoDto {
+    private String image;
+    private String name;
+    private int exp;
+    private int power;
+    private int performanceCnt;
+    private Detail detail;
+
+    public ResponseInfoDto(PocketMon pocketMon){
+        this.image = PocketMonInfo.getByName(pocketMon.getName()).getShapeCode();
+        this.name = pocketMon.getName();
+        this.exp = pocketMon.getExp();
+        this.power = pocketMon.getPower();
+        this.performanceCnt = pocketMon.getPerformanceCnt();
+        this.detail = pocketMon.getDetail();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getPerformanceCnt() {
+        return performanceCnt;
+    }
+
+    public Detail getDetail() {
+        return detail;
+    }
+}
