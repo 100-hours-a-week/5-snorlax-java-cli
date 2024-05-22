0c9933bd87cfc1b2f9370f605e5b93d8b2634063
limzzum <chfhddl1234@naver.com>
2024-05-23 04:27:31 +0900

    :sparkles: feat : 구분가능한 num 추가

--- src/com/java/pocketmon/domain/gym/constant/AttackPoint.java
+++ src/com/java/pocketmon/domain/gym/constant/AttackPoint.java
@@ -1,5 +1,16 @@
 package com.java.pocketmon.domain.gym.constant;
 
+import java.util.Arrays;
+
 public enum AttackPoint {
-    HEAD, BODY, LEG
+    HEAD(1), BODY(2), LEG(3);
+
+    private final int num;
+    AttackPoint(int num){
+        this.num = num;
+    }
+
+    public static AttackPoint getByNum(int num){
+        return Arrays.stream(AttackPoint.values()).filter(e -> e.num == num).findFirst().orElseThrow();
+    }
 }
