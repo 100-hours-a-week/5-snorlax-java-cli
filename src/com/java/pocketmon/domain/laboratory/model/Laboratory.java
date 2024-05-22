cb0541ff91058b3718b8e86c44c408ea9b53aebb
limzzum <chfhddl1234@naver.com>
2024-05-23 04:28:55 +0900

    :sparkles: feat : 상품목록 초기화, 상품 정보 가져오기 메서드 추가

--- src/com/java/pocketmon/domain/laboratory/model/Laboratory.java
+++ src/com/java/pocketmon/domain/laboratory/model/Laboratory.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class Laboratory {
     private List<Product> products;
 
-    public Laboratory(List<Product> products){
-        this.products = products;
+    public Laboratory(){
+        this.products = List.of(new Product("간식", 100, "간식을 먹으면 춤이 저절로 나와요"), new Product("포켓몬 볼", 200, "언젠가 숲 속에 들어가면 포켓몬을 만날지도 몰라요"));
+    }
+
+    public ProductsDto getProductInfo(int num){
+        return new ProductsDto(products.get(num-1).getName(), products.get(num-1).getPrice());
     }
 
     public List<ProductsDto> getProducts(){
