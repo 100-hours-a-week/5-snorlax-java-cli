3b5eac784405e41d914eb57085618918adaf61ca
limzzum <chfhddl1234@naver.com>
2024-05-23 04:27:39 +0900

    :sparkles: feat : 구분가능한 num 추가

--- src/com/java/pocketmon/domain/pocketmon/constant/Detail.java
+++ src/com/java/pocketmon/domain/pocketmon/constant/Detail.java
@@ -1,26 +1,34 @@
 package com.java.pocketmon.domain.pocketmon.constant;
 
+import java.util.Arrays;
+
 public enum Detail {
 
-    MOBUGI("모부기", Type.GRASS,"초록", 0.4f, 10.2f),
-    FIRE_SOONGE("불꽃숭이", Type.FIRE, "갈색", 0.5f, 6.2f),
-    PANGDORI("팽도리", Type.WATER, "파랑", 0.4f, 5.2f),
-    SUPER_MOBUGI("수풀부기", Type.GRASS,"초록", 1.1f, 97f),
-    SUPER_FIRE_SOONGE("파이숭이", Type.FIRE, "갈색", 0.9f, 22f),
-    SUPER_PANGDORI("팽태자", Type.WATER, "파랑", 0.8f, 23f);
+    MOBUGI(1, "모부기", Type.GRASS,"초록", 0.4f, 10.2f),
+    FIRE_SOONGE(2, "불꽃숭이", Type.FIRE, "갈색", 0.5f, 6.2f),
+    PANGDORI(3, "팽도리", Type.WATER, "파랑", 0.4f, 5.2f),
+    SUPER_MOBUGI(4, "수풀부기", Type.GRASS,"초록", 1.1f, 97f),
+    SUPER_FIRE_SOONGE(5, "파이숭이", Type.FIRE, "갈색", 0.9f, 22f),
+    SUPER_PANGDORI(6, "팽태자", Type.WATER, "파랑", 0.8f, 23f);
 
+    private final int num;
     private final String name;
     private final Type type;
     private final String color;
     private final float height;
     private final float weight;
 
-    Detail(String name, Type type, String color, float height, float weight){
+    Detail(int num, String name, Type type, String color, float height, float weight){
+        this.num = num;
         this.name = name;
         this.type = type;
         this.color = color;
         this.height = height;
         this.weight = weight;
+    }
+
+    public static Detail getByNum(int num){
+        return Arrays.stream(Detail.values()).filter(e -> e.num == num).findFirst().orElseThrow();
     }
 
     public String getName() {
@@ -43,21 +51,3 @@ public enum Detail {
         return weight;
     }
 }
-
-//enum Type {
-//    WATER("물", 1), FIRE("불", 2), GRASS("풀", 3);
-//
-//    private final String name;
-//    private final int priority;
-//    Type(String name, int priority){
-//        this.name = name;
-//        this.priority = priority;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//    public int getPriority() {
-//        return priority;
-//    }
-//}
